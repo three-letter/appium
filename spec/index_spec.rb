4863f626331d66b57f5cb05a7e40df855e9b67df
Halsey <Houxi.Chen@dealglobe.com>
2017-01-09 09:34:58 +0800

    meger spec file

--- spec/index_spec.rb
+++ spec/index_spec.rb
@@ -1,27 +1,44 @@
 
-describe 'Index View' do
-  before(:each) do
-    @driver.navigate.to 'http://localhost:3000'
-    @screenshot_id = rand(1000000)   
-  end
+describe 'Dg Web In Appium' do
+  describe 'Index View' do
+    before(:each) do
+      @driver.navigate.to 'http://localhost:3000'
+      @screenshot_id = rand(1000000)   
+    end
 
-  it 'should be have two button' do
-    wait{
-      btns = @driver.find_elements(:class, "btn-dg-info")
+    it 'should be have two button' do
+      wait{
+        btns = @driver.find_elements(:class, "btn-dg-info")
+        @driver.save_screenshot("/Users/andy/Work/appium/images/iv_#{@screenshot_id}.png")
+        expect(btns.size).to eq(2)
+      }
+    end
+
+    it 'should display btn in bottom when swipe down' do
+      @driver.execute_script("window.scrollBy(0, window.innerHeight);")
       @driver.save_screenshot("/Users/andy/Work/appium/images/iv_#{@screenshot_id}.png")
-      expect(btns.size).to eq(2)
-    }
+      btns = @driver.find_elements(:class, "goinvest")
+      btn = btns[0]
+      expect(btn.displayed?).to be true
+    end
+
   end
 
-  it 'should display btn in bottom when swipe down' do
-    @driver.execute_script("window.scrollBy(0, window.innerHeight);")
-    @driver.save_screenshot("/Users/andy/Work/appium/images/iv_#{@screenshot_id}.png")
-    btns = @driver.find_elements(:class, "goinvest")
-    btn = btns[0]
-    expect(btn.displayed?).to be true
+
+
+  describe 'Qa View' do
+    before(:each) do
+      @driver.navigate.to 'http://localhost:3000/invest/qa'
+      @screenshot_id = rand(1000000)   
+    end
+
+    it 'should show 18 items' do
+      items = @driver.find_elements(:class, "panel_list_item")
+      @driver.save_screenshot("/Users/andy/Work/appium/images/iv_#{@screenshot_id}.png")
+      expect(items.size).to eq(18)
+    end
+
   end
+
 
 end
-
-
-
